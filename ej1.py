faa9bba58c6d63472db51de5df26157aa1e21671
Lucas Emery <lucas.emery@hotmail.com>
2020-05-11 16:11:25 -0300

    Fix ej1

--- ej1.py
+++ ej1.py
@@ -1,10 +1,11 @@
 from Bio import SeqIO
 from Bio import SeqRecord
+import re
 
 gbk_filename = "sequences/NC_000207-3.gbk"
 fas_filename = "sequences/NC_000207-3_translated.fasta"
 table = 1
-min_pro_len = 1
+min_pro_len = 50
 only_save_longest = True
 
 with open(gbk_filename, 'r') as gb_handle:
@@ -14,14 +15,18 @@ with open(gbk_filename, 'r') as gb_handle:
                 for frame in range(3):
                     length = 3 * ((len(record) - frame) // 3)  # Multiple of three
                     longest_pro = (0, None)
-                    for pro in nuc[frame:frame + length].translate(table).split("*"):
-                        pro_len = len(pro)
-                        if pro_len >= min_pro_len:
-                            if pro_len > longest_pro[0]:
-                                longest_pro = (pro_len, pro)
-                            elif not only_save_longest:
-                                r = SeqRecord.SeqRecord(pro, id=record.id, description=record.description+' translation')
-                                fas_handle.write(r.format('fasta'))
+                    for trans in nuc[frame:frame + length].translate(table).split("*"):
+                        for m in re.finditer('M', str(trans)):
+                            pro = trans[m.start():]
+                            pro_len = len(pro)
+                            if pro_len >= min_pro_len:
+                                if pro_len > longest_pro[0]:
+                                    longest_pro = (pro_len, pro)
+                                elif not only_save_longest:
+                                    r = SeqRecord.SeqRecord(pro, id=record.id,
+                                        description=record.description+' translation - frame {0}'%(frame*strand))
+                                    fas_handle.write(r.format('fasta'))
                     if longest_pro[1] is not None:
-                        r = SeqRecord.SeqRecord(longest_pro[1], id=record.id, description=record.description + ' translation')
+                        r = SeqRecord.SeqRecord(longest_pro[1], id=record.id,
+                            description=record.description+' translation')
                         fas_handle.write(r.format('fasta'))
