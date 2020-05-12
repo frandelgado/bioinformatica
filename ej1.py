0d77fbd35e4eb33e17eb66181ff8a6443cf9ceb2
Lucas Emery <lucas.emery@hotmail.com>
2020-05-11 22:55:14 -0300

    Added huntingtin gene

--- ej1.py
+++ ej1.py
@@ -2,8 +2,8 @@ from Bio import SeqIO
 from Bio import SeqRecord
 import re
 
-gen = "hemoglobin"
-gbk_filename = "sequences/"+gen+".gbk"
+gen = "huntingtin"
+gbk_filename = "sequences/"+gen+".gb"
 fas_filename = "sequences/"+gen+"_translated.fasta"
 table = 1
 min_pro_len = 50
@@ -12,10 +12,10 @@ only_save_longest = True
 with open(gbk_filename, 'r') as gb_handle:
     with open(fas_filename, 'w') as fas_handle:
         for record in SeqIO.parse(gb_handle, "genbank"):
+            longest_pro = (0, None)
             for strand, nuc in [(+1, record.seq), (-1, record.seq.reverse_complement())]:
                 for frame in range(3):
                     length = 3 * ((len(record) - frame) // 3)  # Multiple of three
-                    longest_pro = (0, None)
                     for trans in nuc[frame:frame + length].translate(table).split("*"):
                         for m in re.finditer('M', str(trans)):
                             pro = trans[m.start():]
@@ -27,7 +27,7 @@ with open(gbk_filename, 'r') as gb_handle:
                                     r = SeqRecord.SeqRecord(pro, id=record.id,
                                         description=record.description+' translation - frame {0}'%(frame*strand))
                                     fas_handle.write(r.format('fasta'))
-                    if longest_pro[1] is not None:
-                        r = SeqRecord.SeqRecord(longest_pro[1], id=record.id,
-                            description=record.description+' translation')
-                        fas_handle.write(r.format('fasta'))
+            if longest_pro[1] is not None:
+                r = SeqRecord.SeqRecord(longest_pro[1], id=record.id,
+                    description=record.description+' translation')
+                fas_handle.write(r.format('fasta'))
